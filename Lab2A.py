4fb85f66f5451060121db8c1b52cbbb7d3d07793
Giovanni Hines <gfhines07@outlook.com>
2025-11-05 23:14:09 -0500

    Remove comments from Lab2A.py

--- Lab2A.py
+++ Lab2A.py
@@ -1,12 +1,7 @@
-# Class: CSE 1321L
-# Section: 01
-# Term: Fall 2025
-# Instructor: Michael Butler
-# Name: Giovanni Hines
-# Lab#: 2A
 
 name = input("Enter a name: ")
 name2 = input("Enter another name: ")
 verb = input("Enter a verb: ")
 adverb = input("Enter an adverb: ")
 print(f"Once upon a time, there was a person named {name} who had a child named {name2}. This child would {verb} {adverb} while singing to strangers.")
+
