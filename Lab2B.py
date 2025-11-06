41b0fd789be437c7807ba9929c010f57e18b96d8
Giovanni Hines <gfhines07@outlook.com>
2025-11-05 23:14:18 -0500

    Remove header comments from Lab2B.py

--- Lab2B.py
+++ Lab2B.py
@@ -1,8 +1,3 @@
-# Class: CSE 1321L
-# Section: 01
-# Term: Fall 2025
-# Instructor: Michael Butler
-# Name: Giovanni Hines
-# Lab#: 2B
+
 
 print("___*___\n__*_*__\n_*_*_*_\n*_*_*_*\n_*_*_*_\n__*_*__\n___*___")
