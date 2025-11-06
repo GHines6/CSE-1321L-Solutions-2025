7a87dcc97e87d22817d77ee2ad84c98d69d33776
Giovanni Hines <gfhines07@outlook.com>
2025-11-05 23:14:49 -0500

    Update Lab3A.py

--- Lab3A.py
+++ Lab3A.py
@@ -1,9 +1,3 @@
-# Class: CSE 1321L
-# Section: 01
-# Term: Fall 2025
-# Instructor: Michael Butler
-# Name: Giovanni Hines
-# Lab#: 3A
 
 AmountOwed = float(input("Amount owed: $"))
 AnnualPercentageRate = float(input("APR: "))
@@ -11,3 +5,4 @@ MonthlyPercentageRate = float(float(AnnualPercentageRate) / 12)
 MinimumPayment = float(AmountOwed * MonthlyPercentageRate / 100)
 print(f"Monthly percentage rate: {round(float(MonthlyPercentageRate), 3)}")
 print(f"Minimum payment: ${round(float(MinimumPayment), 2)}")
+
