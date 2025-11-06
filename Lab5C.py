89d3fa4f8bf2bf3c933df0d1e27c195b288da47d
Giovanni Hines <gfhines07@outlook.com>
2025-11-05 23:16:18 -0500

    Remove comments and clean up Lab5C.py

--- Lab5C.py
+++ Lab5C.py
@@ -1,11 +1,6 @@
-# Class: CSE 1321L
-# Section: 01
-# Term: Fall 2025
-# Instructor: Michael Butler
-# Name: Giovanni Hines
-# Lab#: 5C
 
 response = input('If you would like to stop this program, say "please": ')
 while response != "please":
     response = input('If you would like to stop this program, say "please": ')
+
 print("Program complete")
