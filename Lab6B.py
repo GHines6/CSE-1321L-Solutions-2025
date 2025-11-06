60fc5a27add64d502e57ebd307ae6e52f08fa416
Giovanni Hines <gfhines07@outlook.com>
2025-11-05 23:16:31 -0500

    Remove comments and clean up Lab6B.py

--- Lab6B.py
+++ Lab6B.py
@@ -1,9 +1,3 @@
-# Class: CSE 1321L
-# Section: 01
-# Term: Fall 2025
-# Instructor: Michael Butler
-# Name: Giovanni Hines
-# Lab#: 6B
 
 import random
 random_number = random.randint(1, 100)
@@ -16,3 +10,4 @@ while number != random_number:
         print("Too low!")
     number = int(input("Enter any number between 1 and 100: "))
 print(f"Correct! I was thinking of {random_number}")
+
